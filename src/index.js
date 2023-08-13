805ac3de1c11e88c9f2442152abc1a0326d161da
Viktoriia Zelenska <vikazgit@gmail.com>
2023-08-12 21:41:28 -0700

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    // 
+    // write your solution here         10 = '.'    11 = '-'
+    let str = '';
+    let result = '';
+
+    for (let i = 0; i < expr.length; i++) {
+        str += expr[i];
+
+        if (str.length == 10) {
+            if (str === '**********') {
+                result += ' ';
+            }
+            else {
+                let key = '';
+                for (let j = 0; j < str.length - 1; j+=2) {
+                    if (str[j] === '1' && str[j+1] === '0') {
+                        key += '.';
+                    } 
+                    else if (str[j] === '1' && str[j + 1] === '1') {
+                        key += '-';
+                    }
+                }
+                result += MORSE_TABLE[key];
+            }
+            str = '';
+        }
+
+   }
+    return result;
 }
+
+// console.log(decode('0000001111**********000000001000000011110000000010'));
 
 module.exports = {
     decode
